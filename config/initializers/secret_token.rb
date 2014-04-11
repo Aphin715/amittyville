b7bcb9ea79fc3e02329a3ad648f1344d73e6339a
Alex Phinizy <AlexPhinizy@gmail.com>
2014-04-11 17:20:28 -0400

    initial commit-

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Amityville::Application.config.secret_key_base = 'da4a49d425c41fb0a71527cd2b5b1b2069620609be685c32176ff3c2e5926e83900680ebe5ccf3ea60b50b5a5bc75052261df0c979f1d4099a0f6c5f9a4b0b59'
