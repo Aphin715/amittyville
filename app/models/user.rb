73f4c6e4b5d0d0639b06e760bfe3d9e5cd37b722
Alex Phinizy <AlexPhinizy@gmail.com>
2014-04-28 17:21:03 -0400

    added missing classmates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+
   mount_uploader :user_photo, UserPhotoUploader
+
+   def name
+    "#{first_name} #{last_name}"
+  end
 end
